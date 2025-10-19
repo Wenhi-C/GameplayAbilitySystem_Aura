72baf62f8dcf98f0efcfc411798081891cdee7e7
Wenhi <810319935@qq.com>
2025-10-19 23:51:11 +0800

    Mapping Tags to Attributes

--- Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
+++ Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
@@ -50,6 +50,10 @@ struct FEffectProperties
 	
 	
 };
+// typedef is specific to the FGameplayAttribute() signature, but TStatciFuncPtr is generic to any signature chosen
+typedef TBaseStaticDelegateInstance<FGameplayAttribute(), FDefaultDelegateUserPolicy>::FFuncPtr FAttributeFuncPtr;
+template<class T>
+using TStaticFuncPtr = typename TBaseStaticDelegateInstance<T, FDefaultDelegateUserPolicy>::FFuncPtr;
 
 /**
  * 
@@ -65,6 +69,10 @@ public:
 	virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
 	virtual void PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data) override;
 
+	TMap<FGameplayTag , TStaticFuncPtr<FGameplayAttribute()>> TagsToAttributes;
+
+	
+	
 	// Primary Attributes
 	
 	UPROPERTY(BlueprintReadOnly ,ReplicatedUsing = OnRep_Strength, Category = "Primary Attributes")
