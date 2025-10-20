417435da9e09b315a778731fa0c4452bcf681174
Wenhi <810319935@qq.com>
2025-10-20 16:44:46 +0800

    Input Config Data Asset

---
+++ Source/Aura/Private/Input/AuraInputConfig.cpp
@@ -0,0 +1,21 @@
+// Copyright Wenhi
+
+
+#include "Input/AuraInputConfig.h"
+
+const UInputAction* UAuraInputConfig::FindAbilityInputActionForTag(const FGameplayTag& InputTag,
+	bool bLogNotFound) const
+{
+	for (const FAuraInputAction& AbilityInputAction : AbilityInputActions)
+	{
+		if (AbilityInputAction.InputAction && AbilityInputAction.InputTag == InputTag)
+		{
+			return AbilityInputAction.InputAction;
+		}
+	}
+	if (bLogNotFound)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Can't find AbilityInputAction for InputTag [%s], on InputConfig [%s]."), *InputTag.ToString(), *GetNameSafe(this));
+	}
+	return nullptr;
+}
