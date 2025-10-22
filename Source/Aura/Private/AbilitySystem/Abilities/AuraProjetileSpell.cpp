5d5ad137b9ba0c394f77b6396eff132a2826bd39
Wenhi <810319935@qq.com>
2025-10-22 22:48:09 +0800

    Aura Projectile Spell

---
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraProjetileSpell.cpp
@@ -0,0 +1,13 @@
+// Copyright Wenhi
+
+
+#include "AbilitySystem/Abilities/AuraProjetileSpell.h"
+
+void UAuraProjetileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+	const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+	
+}
---
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraProjetileSpell.h
@@ -0,0 +1,18 @@
+ // Copyright Wenhi
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/AuraGameplayAbility.h"
+#include "AuraProjetileSpell.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraProjetileSpell : public UAuraGameplayAbility
+{
+	GENERATED_BODY()
+protected:
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+};
