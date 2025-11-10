c653deebda55fa1a98f0bdfc820783d367114802
Wenhi <810319935@qq.com>
2025-11-10 16:47:08 +0800

    Behavior Tree Service

---
+++ Source/Aura/Private/AI/BTService_FindNearestPlayer.cpp
@@ -0,0 +1,9 @@
+// Copyright Wenhi
+
+
+#include "AI/BTService_FindNearestPlayer.h"
+
+void UBTService_FindNearestPlayer::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+}
